8630cc22bbbb36a22119d27946bda13363bd11d2
陈鸿chhhh <15168264355@163.com>
2018-11-13 14:46:58 +0800

    commit resource module

---
+++ app/src/main/java/com/ch/android/mavenmodule/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ch.android.mavenmodule;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.google.zxing.view.CapturePage;
+
+/**
+ * Created by mac on 2018/11/10.
+ */
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CapturePage capturePage=new CapturePage(this,null);
+    }
+}
