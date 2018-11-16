d483120bfb82fb02b088a7d94823ced3cacae040
a568261070 <woshichenhong12@gmail.com>
2018-11-16 18:06:43 +0800

    提交

--- app/src/main/java/com/ch/android/mavenmodule/MainActivity.java
+++ app/src/main/java/com/ch/android/mavenmodule/MainActivity.java
@@ -14,6 +14,7 @@ import com.ch.android.cssdk.service.MyTestService;
 /**
  * Created by mac on 2018/11/10.
  */
+
 @ActivityScope
 public class MainActivity extends com.ch.android.common.base.BaseActivity {
 
@@ -31,7 +32,7 @@ public class MainActivity extends com.ch.android.common.base.BaseActivity {
     public void initData(@Nullable Bundle bundle) {
         Log.d(getClass().getSimpleName(),"initData");
 
-
+        
 
 
 
