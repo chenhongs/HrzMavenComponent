a78a0db90a298f031a0079a54b75ce37c0dd5ebe
陈鸿 <15168264355@163.com>
2018-11-20 22:51:06 +0800

    push

---
+++ app/src/main/java/com/ch/android/mavenmodule/DemoIntentService.java
@@ -0,0 +1,68 @@
+package com.ch.android.mavenmodule;
+//Thanks For Your Reviewing My Code 
+//Please send your issues email to 15168264355@163.com when you find there are some bugs in My class 
+//You Can add My wx 17620752830 and we can communicate each other about IT industry
+//Code Programming By MrCodeSniper on 2018/11/17.Best Wishes to You!  []~(~▽~)~* Cheers!
+
+
+import android.content.Context;
+import android.util.Log;
+
+import com.igexin.sdk.GTIntentService;
+import com.igexin.sdk.message.GTCmdMessage;
+import com.igexin.sdk.message.GTNotificationMessage;
+import com.igexin.sdk.message.GTTransmitMessage;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * 继承 GTIntentService 接收来自个推的消息, 所有消息在线程中回调, 如果注册了该服务, 则务必要在 AndroidManifest中声明, 否则无法接受消息<br>
+ * onReceiveMessageData 处理透传消息<br>
+ * onReceiveClientId 接收 cid <br>
+ * onReceiveOnlineState cid 离线上线通知 <br>
+ * onReceiveCommandResult 各种事件处理回执 <br>
+ */
+public class DemoIntentService extends GTIntentService {
+
+    public DemoIntentService() {
+
+    }
+
+    @Override
+    public void onReceiveServicePid(Context context, int pid) {
+    }
+
+    @Override
+    public void onReceiveMessageData(Context context, GTTransmitMessage msg) {
+        try {
+            String s=new String(msg.getPayload(),"utf-8");
+            Log.e("xxx",s+"aaa");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onReceiveClientId(Context context, String clientid) {
+        Log.e("xxx", "onReceiveClientId -> " + "clientid = " + clientid);
+    }
+
+    @Override
+    public void onReceiveOnlineState(Context context, boolean online) {
+        Log.e("xxx",online+"bbb");
+    }
+
+    @Override
+    public void onReceiveCommandResult(Context context, GTCmdMessage cmdMessage) {
+        Log.e("xxx",cmdMessage.getAction()+"ccc");
+    }
+
+    @Override
+    public void onNotificationMessageArrived(Context context, GTNotificationMessage msg) {
+        Log.e("xxx",msg.getContent()+"ddd");
+    }
+
+    @Override
+    public void onNotificationMessageClicked(Context context, GTNotificationMessage msg) {
+    }
+}
