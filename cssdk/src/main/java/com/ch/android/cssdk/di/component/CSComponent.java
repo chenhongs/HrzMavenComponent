7fba54872672ac790d27eabc666ea6652f8c7abc
panrongfu <511752921@qq.com>
2018-11-23 16:55:16 +0800

    客服模块

--- cssdk/src/main/java/com/ch/android/cssdk/di/component/CSComponent.java
+++ cssdk/src/main/java/com/ch/android/cssdk/di/component/CSComponent.java
@@ -1,14 +1,12 @@
 package com.ch.android.cssdk.di.component;
 
-import com.ch.android.common.dagger.component.AppComponent;
-import com.ch.android.common.dagger.scope.ActivityScope;
 import com.ch.android.cssdk.di.module.CSModule;
 import com.ch.android.cssdk.mvp.ui.activity.CSActivity;
 
 import dagger.Component;
 
-@ActivityScope
-@Component(modules = CSModule.class, dependencies = AppComponent.class)
+
+@Component(modules = CSModule.class)
 public interface CSComponent {
     void inject(CSActivity activity);
 }
